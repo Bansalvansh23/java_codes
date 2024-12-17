47d41add78e55daaf3927888f138aeab93806dea
Vansh Bansal <vanshbansal030@gmail.com>
2024-12-17 11:52:28 +0530

    Concept of 2D array in java

---
+++ TwoDarray.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+public class TwoDarray {
+    public static void main(String[] args) {
+        int matrix[][] = new int[3][3];
+        input(matrix);
+        // output(matrix);
+        // search(matrix);
+        System.out.println(findMax(matrix));
+        System.out.println(findMin(matrix));
+    }
+
+    public static void input(int[][] matrix) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        sc.close();
+    }
+
+    public static void output(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean search(int[][] matrix) {
+        int key = 5;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == key) {
+                    System.out.println("The key found at" + "(" + i + "," + j + ")");
+                    return true;
+                }
+            }
+        }
+        System.out.println("Key not found");
+        return false;
+    }
+
+    public static int findMax(int[][] matrix) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (max < matrix[i][j]) {
+                    max = matrix[i][j];
+                }
+            }
+        }
+        return max;
+    }
+
+    public static int findMin(int[][] matrix) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (min > matrix[i][j]) {
+                    min = matrix[i][j];
+                }
+            }
+        }
+        return min;
+    }
+}
