eeb0b56ae4e87be0d24239f6abab40539d6c92b3
Vansh Bansal <vanshbansal030@gmail.com>
2025-01-13 18:30:38 +0530

    demo code for bit manipulation

--- BitManipulation.java
+++ BitManipulation.java
@@ -1,14 +1,14 @@
 public class BitManipulation {
-    public static void OddOrEven(int n){
+    public static void OddOrEven(int n) {
         int bitmask = 1;
-        if((n & bitmask)==0){
+        if ((n & bitmask) == 0) {
             System.out.println(n + " is even");
-        }
-        else{
+        } else {
             System.out.println(n + " is odd");
         }
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         OddOrEven(2);
         OddOrEven(71);
     }
