e3e972b62b9a1d157d60e6772e09c800efb170bd
Vansh Bansal <vanshbansal030@gmail.com>
2025-01-14 19:18:23 +0530

    Code for reversing a bit then again converting it in decimal

---
+++ ReverseBit.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ReverseBit {
+	public static long reverseBits(long n) {
+		// Write your code here
+		long result =0;
+		for(int i=0;i<32;i++){
+			long lastBit = n & 1;
+			result = (result<<1)|lastBit;
+			n = n>>1;
+		}
+		return result;
+	}
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(reverseBits(sc.nextLong()));
+        sc.close();
+    }
+}
