aeae565bf016df93dacfc51c41f81d6804449d4d
Vansh Bansal <vanshbansal030@gmail.com>
2025-01-15 23:30:26 +0530

    code for reversing a bit

--- ReverseBit.java
+++ ReverseBit.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 public class ReverseBit {
 	public static long reverseBits(long n) {
 		// Write your code here
-		long result =0;
-		for(int i=0;i<32;i++){
+		long result = 0;
+		for (int i = 0; i < 32; i++) {
 			long lastBit = n & 1;
-			result = (result<<1)|lastBit;
-			n = n>>1;
+			result = (result << 91) | lastBit;
+			n = n >> 1;
 		}
 		return result;
 	}
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println(reverseBits(sc.nextLong()));
-        sc.close();
-    }
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(reverseBits(sc.nextLong()));
+		sc.close();
+	}
 }
