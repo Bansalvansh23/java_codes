27882f29e0e2a5ec0300df1f88278e6a417c3d5a
Vansh Bansal <vanshbansal030@gmail.com>
2025-01-15 23:32:17 +0530

    Code for stock problem

--- StockProblem.java
+++ StockProblem.java
@@ -2,21 +2,22 @@
 // The Time Complexity of this problem is O(n).
 
 public class StockProblem {
-    public static void main(String args[]){
-        int price[] = {7,1,5,3,6,4};
+    public static void main(String args[]) {
+        int price[] = { 7, 1, 5, 3, 6, 4 };
         System.out.println(BuySellStocks(price));
     }
-    public static int BuySellStocks(int price[]){
-        int buyPrice = Integer.MAX_VALUE;
-        int maxProfit  = 0;
 
-        // For calculating Profit 
-        
-        for(int i=0;i<price.length;i++){
-            if(buyPrice < price[i]){
-                int profit = price[i]-buyPrice;
-                maxProfit = Math.max(maxProfit,profit);
-            }else{
+    public static int BuySellStocks(int price[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        // For calculating Profit
+
+        for (int i = 0; i < price.length; i++) {
+            if (buyPrice < price[i]) {
+                int profit = price[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
                 buyPrice = price[i];
             }
         }
