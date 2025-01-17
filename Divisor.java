aa9e629aeca8b1e338b3e07574dad0727a7e6c48
Vansh Bansal <vanshbansal030@gmail.com>
2025-01-17 19:08:15 +0530

    Code for finding sum of divisor upto N

--- Divisor.java
+++ Divisor.java
@@ -1,12 +1,9 @@
-import java.util.ArrayList;
-//import java.util.Arrays;
-
 public class Divisor {
     public static void main(String[] args) {
-        int n =3;
-        int res=0;
-        for(int i=1;i<=n;i++){
-            res += ((n/i)*i);
+        int n = 3;
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            res += ((n / i) * i);
         }
         System.out.println(res);
     }
