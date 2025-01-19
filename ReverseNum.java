184deedf533a81ee36adf233d7724ec1863f5cce
Vansh Bansal <vanshbansal030@gmail.com>
2025-01-19 17:04:27 +0530

    Code for reversing a number

--- ReverseNum.java
+++ ReverseNum.java
@@ -16,18 +16,19 @@
 /// Another method for finding reverse of a number 
 
 import java.util.Scanner;
+
 public class ReverseNum {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number you want to reverse: ");
         int num = sc.nextInt();
-        int rev =0;
-        while(num!=0){
-            int lastDigit = num %10;
-            rev = ((rev*10)+lastDigit);
-            num = num /10;
+        int rev = 0;
+        while (num != 0) {
+            int lastDigit = num % 10;
+            rev = ((rev * 10) + lastDigit);
+            num = num / 10;
         }
-        System.out.println("Reverse of the number is: "+rev);
+        System.out.println("Reverse of the number is: " + rev);
         sc.close();
     }
 }
