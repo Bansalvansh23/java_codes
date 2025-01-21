cfead492dbed84236acdb4c1b5c8de67b4c42650
Vansh Bansal <vanshbansal030@gmail.com>
2025-01-21 22:46:20 +0530

    Code for stair case searching algo

--- StairCaseSearching.java
+++ StairCaseSearching.java
@@ -1,3 +1,5 @@
+// this is the best and optimal approach for searching in matrix
+
 public class StairCaseSearching {
     public static void main(String[] args) {
         int[][] matrix = { { 10, 20, 30, 40 }, { 15, 25, 35, 45 }, { 27, 29, 37, 48 }, { 32, 33, 39, 50 } };
