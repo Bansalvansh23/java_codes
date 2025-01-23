a2e030ddc3bc82ccd4782c2207d4d7b3486cf148
Vansh Bansal <vanshbansal030@gmail.com>
2025-01-23 22:21:40 +0530

    Example code for all four operations in hashmap

--- HashOperation.java
+++ HashOperation.java
@@ -4,24 +4,24 @@ public class HashOperation {
     public static void main(String[] args) {
         HashMap<String, Integer> map = new HashMap<>();
 
-        // insert operation in  hash
-        map.put("India",100);
-        map.put("USA",50);
-        map.put("UK",200);
+        // insert operation in hash
+        map.put("India", 100);
+        map.put("USA", 50);
+        map.put("UK", 200);
 
         System.out.println(map);
 
-        //get operation in hash
-        System.out.println(map.get("India")); //100
-        System.out.println(map.get("Pakistan"));  // nulll
+        // get operation in hash
+        System.out.println(map.get("India")); // 100
+        System.out.println(map.get("Pakistan")); // nulll
 
         // contians operation to check the presence of key
-        System.out.println(map.containsKey("USA"));  // true
-        System.out.println(map.containsKey("Pakistan"));   //false
+        System.out.println(map.containsKey("USA")); // true
+        System.out.println(map.containsKey("Pakistan")); // false
 
         // remove operation in hash
         System.out.println(map.remove("UK"));
         System.out.println(map);
-        System.out.println(map.remove("Pakistan")); //null
+        System.out.println(map.remove("Pakistan")); // null
     }
 }
