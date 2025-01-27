9f7c21fec35fb815a4b4debf1b9c35d7184bb37f
Vansh Bansal <vanshbansal030@gmail.com>
2025-01-27 19:36:41 +0530

    Basic operations on stack

---
+++ stack/BasicStack.java
@@ -0,0 +1,23 @@
+package stack;
+import java.util.Stack;
+public class BasicStack {
+    public static void main(String[] args){
+        Stack<Integer> stack = new Stack<>();
+
+        // to add element in the stack
+        stack.push(10);
+        stack.push(20);
+        System.out.println(stack);
+
+        // to check the topmost element of the stack
+        System.out.println(stack.peek());
+
+        // to remove the topmost element from the stack
+        stack.pop();
+        System.out.println(stack);
+
+        // to check whether the stack is empty or not
+        System.out.println(stack.isEmpty());
+
+    }
+}
